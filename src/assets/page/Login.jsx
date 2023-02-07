cd742cbe5b94869076616887a27bf95f78ec44f2
DarioCastel <lecastelnovo@gmail.com>
2023-02-07 20:26:49 -0300

    girasol-login

---
+++ src/assets/page/Login.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./../css/login.scss";
+import Avatar from "./../Img/avatar.jpg";
+
+const Login = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="login">
+          <div className="avatar">
+            <img src={Avatar} alt="avatar" />
+          </div>
+          <h2>Login</h2>
+          <h3>Welcome</h3>
+          <form className="login-form">
+            <div className="textbox">
+              <input type="email" placeholder="UserName" />
+              <span className="material-symbols-outlined"><i class="fa-solid fa-user"></i></span>
+            </div>
+            <div className="textbox">
+              <input type="password" placeholder="Password" />
+              <span className="material-symbols-outlined"><i className="fa-solid fa-lock"></i></span>
+            </div>
+            <button type="submit">LOGIN</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
